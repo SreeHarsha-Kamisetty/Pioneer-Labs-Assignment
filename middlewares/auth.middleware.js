bcc4642dd922d7b77b2b2c6619d636317eb620f6
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-03-26 16:48:38 +0530

    middleware to authenticate users

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const { BlackListModel } = require("../models/blacklisttoken.model");
+require("dotenv").config();
+
+
+const auth = async(req,res,next)=>{
+
+    try {
+        let token = req.headers.authorization?.split(" ")[1];
+
+        if(!token) return res.status(401).json({Message:"Please login to continue"})
+
+        let expiredToken = await BlackListModel.findOne({accessToken:token});
+
+        if(expiredToken) return res.status(401).json({Message:"Invalid token"})
+
+        let result = jwt.verify(token, process.env.SECRET_KEY);
+
+        if(result) next();
+    } catch (error) {
+        res.status(401).json({Message:"Unauthorized"})
+    }
+
+}
+
+module.exports={
+    auth
+}
